801b0d9766e713b6406159eb2f0142b59bc1060b
Matthew Howey <matthew.howey@yahoo.co.uk>
2026-05-02 10:19:42 +0100

    create controller

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import homePageController from './controllers/home-page.js';
 
 const app = express();
 
@@ -6,6 +7,9 @@ app.use('/', (req, res, next) => {
   console.log(`Call received on my first chatbot: ${req.method} ${req.url}`);
   next();
 });
+
+app.get('/home', homePageController);
+
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log('My first chatbotm, listening on port 3000');
 });
---
+++ controllers/home-page.js
@@ -0,0 +1,5 @@
+const homePageController = (req, res) => {
+  res.send('You are on the home page');
+};
+
+export default homePageController;
