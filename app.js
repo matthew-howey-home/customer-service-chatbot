bb24316e981b881639dfd234d370a451c7954941
Matthew Howey <matthew.howey@yahoo.co.uk>
2026-05-02 20:57:45 +0100

    play initial audio

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ nunjucks.configure('views', {
   express: app
 });
 
+app.use(express.static('public'));
+
 app.use('/', (req, res, next) => {
   console.log(`Call received on my first chatbot: ${req.method} ${req.url}`);
   next();
--- controllers/home-page.js
+++ controllers/home-page.js
@@ -3,8 +3,8 @@ import textToSpeech from '../lib/text-to-speech.js';
 
 const homePageController = async (req, res) => {
   const LLMResponse = await callLLM('');
-  const speechFilePath = await textToSpeech(LLMResponse.speak_to_customer);
-  res.render('home.njk');
+  const speechFileName = await textToSpeech(LLMResponse.speak_to_customer);
+  res.render('home.njk', { initialAudio: `/${speechFileName}` });
 };
 
 export default homePageController;
