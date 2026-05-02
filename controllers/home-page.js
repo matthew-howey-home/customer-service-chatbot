2e9febb803f6f2f87d98d86b3017825470fa1b3c
Matthew Howey <matthew.howey@yahoo.co.uk>
2026-05-02 20:42:36 +0100

    add text to speech

--- controllers/home-page.js
+++ controllers/home-page.js
@@ -1,7 +1,9 @@
 import callLLM from '../lib/call-llm.js';
+import textToSpeech from '../lib/text-to-speech.js';
 
 const homePageController = async (req, res) => {
   const LLMResponse = await callLLM('');
+  const speechFilePath = await textToSpeech(LLMResponse.speak_to_customer);
   res.send('You are on the home page');
 };
 
