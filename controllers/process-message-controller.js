243e21b165d1d75fad353957ab7b923276560edb
Matthew Howey <matthew.howey@yahoo.co.uk>
2026-05-03 08:53:10 +0100

    call LLM with user response

--- controllers/process-message-controller.js
+++ controllers/process-message-controller.js
@@ -3,6 +3,7 @@ import textToSpeech from '../lib/text-to-speech.js';
 
 const processMessageController = async (req, res) => {
   console.log('Message received was:', req.body);
+  callLLM(req.body.message);
 };
 
 export default processMessageController;
