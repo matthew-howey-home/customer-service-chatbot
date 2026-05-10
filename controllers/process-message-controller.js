f5259718ffa3980778e1a9c0afa6a21167493ddc
Matthew Howey <matthew.howey@yahoo.co.uk>
2026-05-10 12:07:15 +0100

    stop conversation when name spelling confirmed

--- controllers/process-message-controller.js
+++ controllers/process-message-controller.js
@@ -4,8 +4,18 @@ import textToSpeech from '../lib/text-to-speech.js';
 const processMessageController = async (req, res) => {
   console.log('Message received was:', req.body);
   const llmResponse = await callLLM(req.body.message);
-  const fileName = await textToSpeech(llmResponse.speak_to_customer);
-  res.json({ audio_url: fileName });
+  const { customer_data } = llmResponse;
+  const { firstName, lastName } = customer_data;
+  if (
+   firstName.correct_spelling && firstName.spelling_confirmed
+   && lastName.correct_spelling && lastName. spelling_confirmed
+  ) {
+    const fileName = await textToSpeech('Thank you for confirming the spelling of your first and last names.');
+    res.json({ audio_url: fileName, stop_conversation: true, customer_data });
+  } else {
+    const fileName = await textToSpeech(llmResponse.speak_to_customer);
+    res.json({ audio_url: fileName });
+  }
 };
 
 export default processMessageController;
